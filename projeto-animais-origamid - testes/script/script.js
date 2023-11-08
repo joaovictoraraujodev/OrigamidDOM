30ece18ce6417c7111e2fec241068ac51cf73580
joaovictoraraujodev <joaovitorsms@hotmail.com>
2023-11-08 11:13:30 -0300

    projeto-teste

---
+++ projeto-animais-origamid - testes/script/script.js
@@ -0,0 +1,30 @@
+const animais = document.getElementById('animais');
+
+const gridSection = document.getElementsByClassName('grid-section');
+const contato = document.getElementsByTagName('grid-section contato');
+
+const ul = document.getElementsByTagName('ul');
+
+console.log(gridSection[0]);
+
+const animais2 = document.querySelector('.animais');
+
+const contato2 = document.querySelector('#contato');
+
+const ultimoItem = document.querySelector('.animais-lista li:last-child');
+
+const linkCSS = document.querySelector('[href^="https://"]');
+
+const primeiroUl = document.querySelector('ul');
+
+const navItem = primeiroUl.querySelector('li');
+
+const gridSection2 = document.querySelectorAll('.grid-section');
+
+const listas = document.querySelectorAll('ul');
+
+const titulos = document.querySelectorAll('.titulo');
+
+const fotosAnimais = document.querySelectorAll('.animais-lista imgs')
+
+console.log(gridSection2[1])
